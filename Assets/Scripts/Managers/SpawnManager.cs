04586e45d6ad78256c392f1e02121dabc6849512
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-27 13:36:10 +0530

    Navmesh Works

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class SpawnAgent
+{
+    public int id;
+    public string name;
+
+    public GameObject prefab;
+}
+
+public class SpawnManager : MonoBehaviour
+{
+    public float m_SpawnRate = 1f; //Spawn Rate in Seconds
+    public int m_MaxAgentsCount = 5;
+
+    [SerializeField] private AgentManager m_agentManager;
+    [SerializeField] private List<SpawnAgent> m_spawnAgentPrefabs;
+    [SerializeField] private Transform m_spawnPoint;
+    private float m_currentTime = 0;
+    private int m_currentAgentsCount = 0;
+
+    private void Update()
+    {
+        m_currentAgentsCount = m_agentManager.m_currentAgents.Count;
+
+        if (m_currentAgentsCount < m_MaxAgentsCount)
+        {
+            SpawnOverTime();
+        }
+    }
+
+    private void SpawnOverTime()
+    {
+        m_currentTime += Time.deltaTime;
+
+        if (m_currentTime > m_SpawnRate)
+        {
+            m_currentTime = 0;
+
+            GameObject spawnedAgent = Instantiate(m_spawnAgentPrefabs[Random.Range(0, m_spawnAgentPrefabs.Count)].prefab,
+                m_spawnPoint.position, Quaternion.identity);
+
+            spawnedAgent.transform.SetParent(m_spawnPoint);
+
+            m_agentManager.m_currentAgents.Add(spawnedAgent.GetComponent<NavMeshAgent>());
+        }
+    }
+}
