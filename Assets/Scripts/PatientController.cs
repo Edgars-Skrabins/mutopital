b644bffd1274a34b82323d9a79f7a218d82e10f5
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-27 15:00:02 +0530

    Bleh

--- Assets/Scripts/PatientController.cs
+++ Assets/Scripts/PatientController.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class PatientController : MonoBehaviour
 {
-
+    [SerializeField] private bool m_healed = false;
     private NavMeshAgent m_navmeshAgent;
     [SerializeField] private float obstacleCheckRadius = 2f;
 
@@ -17,15 +17,12 @@ public class PatientController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // Check if there is an obstacle (another agent) within the specified radius
         if (IsObstacleAround())
         {
-            // Stop the agent when an obstacle is detected
             m_navmeshAgent.isStopped = true;
         }
         else
         {
-            // Continue moving towards the target when no obstacle is around
             m_navmeshAgent.isStopped = false;
         }
     }
