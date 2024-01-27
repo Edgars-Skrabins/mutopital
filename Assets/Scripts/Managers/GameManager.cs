5bc05307595dd9538b3c9b6b6a8957ab91d8dcd5
Edgars Skrabins <shiftkiro@gmail.com>
2024-01-27 12:53:18 +0200

    HighScore

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,7 @@ public class GameManager : Singleton<GameManager>
 
     protected override void Awake()
     {
+        base.Awake();
         Initialize();
     }
 
--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
+public class ScoreManager : Singleton<ScoreManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    private int m_highScore;
+    private int m_score;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+        InitializeHighScore();
+    }
+
+    private void InitializeHighScore()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            m_highScore = PlayerPrefs.GetInt("HighScore");
+            return;
+        }
+
+        m_highScore = 0;
+        PlayerPrefs.SetInt("HighScore", m_highScore);
+    }
+
+    public int GetScore()
+    {
+        return m_score;
+    }
+
+    public int GetHighScore()
+    {
+        return m_highScore;
     }
 }
