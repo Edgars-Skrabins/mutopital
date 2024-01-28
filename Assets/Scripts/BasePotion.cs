865f651c26d874ad99b2162e4930c400ef136e3b
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-28 15:09:56 +0530

    Base Potion Added

--- Assets/Scripts/BasePotion.cs
+++ Assets/Scripts/BasePotion.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasePotion : MonoBehaviour
+public class BasePotion : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Interact()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.SetParent(FindObjectOfType<PlayerInteract>().transform);
     }
 }
