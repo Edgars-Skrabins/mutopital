9311d43474d2cb0bebd36add2a95e59456eda29a
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-28 18:05:31 +0530

    Potion Health And Bunch of Stuff

---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] private Transform m_handTF;
+    [SerializeField] private Transform m_objectsInHand;
+
+
+    private void LateUpdate()
+    {
+        if(m_objectsInHand)
+            m_objectsInHand.position = m_handTF.position;
+    }
+
+    public void PickUpObject(Transform _object)
+    {
+        if (m_objectsInHand == null)
+        {
+            m_objectsInHand = _object;
+            m_objectsInHand.transform.SetParent(m_handTF);
+            m_objectsInHand.position = m_handTF.position;
+        }
+        else
+        {
+            DropObject();
+            m_objectsInHand = _object;
+            m_objectsInHand.SetParent(m_handTF);
+            m_objectsInHand.position = m_handTF.position;
+        }
+    }
+
+    public void DropObject()
+    {
+        Debug.Log("Drop Object!");
+
+        if (m_objectsInHand)
+        {
+            m_objectsInHand.DetachChildren();
+            m_objectsInHand.GetComponent<Rigidbody>().AddForce(transform.forward * 100, ForceMode.Impulse);
+            m_objectsInHand = null;
+        }
+    }
+}
