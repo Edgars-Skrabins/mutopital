b85ce313c3cacf70d12978166ed73bf185cfde0b
Edgars Skrabins <shiftkiro@gmail.com>
2024-01-28 15:07:12 +0200

    Mutation Template + Mutation Controller

--- Assets/Scripts/Patient/MutationController.cs
+++ Assets/Scripts/Patient/MutationController.cs
@@ -4,6 +4,7 @@ using Random = UnityEngine.Random;
 public class MutationController : MonoBehaviour
 {
     [SerializeField] private Mutation[] m_mutations;
+    [SerializeField] private Mutation m_activeMutation;
 
     private void Awake()
     {
@@ -18,6 +19,22 @@ public class MutationController : MonoBehaviour
     private void PickRandomMutation()
     {
         int randomNum = Random.Range(0, m_mutations.Length - 1);
+        m_activeMutation = m_mutations[randomNum];
         m_mutations[randomNum].enabled = true;
+    }
+
+    private void RemoveActiveMutation()
+    {
+        m_activeMutation.enabled = false;
+    }
+
+    public void AttemptRemoveMutation(string _potionName)
+    {
+        if(m_activeMutation.GetMutationHealPotionName() == _potionName)
+        {
+            RemoveActiveMutation();
+        }
+
+        // TODO: Alternative functionality for when potion is wrong
     }
 }
